ea919e0d3c988d344d7d60f3471bb2b6273c2193
thiekokumagai <thieko.kumagai@gmail.com>
2023-01-12 23:27:00 -0400

    Laravel

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/series',function(){
+    return \App\Models\Serie::all();
+});
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Serie;
 use Illuminate\Support\Facades\Route;
 
 /*
